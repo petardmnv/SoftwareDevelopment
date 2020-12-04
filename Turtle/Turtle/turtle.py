3c5f7765b480e44a17bb51aed75650661e682f0f
petardmnv <petar.dmnv@gmail.com>
2020-12-04 22:13:20 +0200

    Add Turtle

--- Turtle/Turtle/turtle.py
+++ Turtle/Turtle/turtle.py
@@ -9,15 +9,19 @@ class Turtle:
     orientation: str
 
     def __init__(self, x: int, y: int):
+        if x <= 0 or y <= 0:
+            raise IndexError
         self.row = x
         self.column = y
-        self.x = 0
-        self.y = 0
         self.canvas = [[0] * y for i in range(x)]
         self.is_spawned = False
         self.orientation = "right"
 
     def spawn_at(self, row: int, column: int):
+        if (row < 0) and (row >= self.row):
+            raise IndexError
+        if (column < 0) and (column >= self.column):
+            raise IndexError
         self.is_spawned = True
         self.x = row
         self.y = column
@@ -49,16 +53,14 @@ class Turtle:
         index: int = orientations.index(self.orientation) + 1
         if index >= len(orientations):
             index = 0
-        self.orientation =\
-            orientations[index]
+        self.orientation = orientations[index]
 
     def turn_left(self):
         orientations: tuple = ("left", "up", "right", "down")
         index: int = orientations.index(self.orientation) -1
         if index < 0:
             index = len(orientations) - 1
-        self.orientation =\
-            orientations[index]
+        self.orientation = orientations[index]
 
 
 class SimpleCanvas:
@@ -69,30 +71,29 @@ class SimpleCanvas:
         self.symbols = symbols
         self.canvas = canvas
 
-    def draw(self): #-> str:
+    def draw(self) -> List[List[str]]:
         canvas_rows: int = len(self.canvas)
         canvas_columns: int = len(self.canvas[0])
-        pixels_count: int = sum([self.canvas[i][j] for j in range(canvas_columns) for i in range(canvas_rows)])
+        pixels_count: int = max([self.canvas[i][j] for j in range(canvas_columns) for i in range(canvas_rows)])
 
-        pixelized_cnavas: List[List[float]] = [[0.00] * canvas_columns for i in range(canvas_rows)]
+        pixelated_canvas: List[List[float]] = [[0.00] * canvas_columns for i in range(canvas_rows)]
+        draw_canvas: List[List[str]] = [[""] * canvas_columns for i in range(canvas_rows)]
+
+        symbols_range: float = 1 / (len(self.symbols) - 1)
         for i in range(canvas_rows):
             for j in range(canvas_columns):
-                pixelized_cnavas[i][j] = self.canvas[i][j] / pixels_count
-        return pixelized_cnavas
+                pixelated_canvas[i][j] = self.canvas[i][j] / pixels_count
 
+                #drawing canvas
+                if 0 == pixelated_canvas[i][j]:
+                    draw_canvas[i][j] = self.symbols[0]
 
-turtle = Turtle(3, 3)
-turtle.spawn_at(0, 0)
-for i in range(9):
-    turtle.move()
-turtle.turn_right()
-for i in range(4):
-    turtle.move()
-turtle.turn_left()
-turtle.move()
-turtle.move()
-turtle.turn_right()
-turtle.move()
-print(turtle.canvas)
-canvas = SimpleCanvas(turtle.canvas, [' ', '*', '@', '#'])
-print(canvas.draw())
+                for k in range(len(self.symbols) - 2):
+                    if (k * symbols_range < pixelated_canvas[i][j]) and \
+                            ((k + 1) * symbols_range >= pixelated_canvas[i][j]):
+                        draw_canvas[i][j] = self.symbols[k + 1]
+
+                if ((1 - symbols_range) < pixelated_canvas[i][j]) and (1 >= pixelated_canvas[i][j]):
+                    draw_canvas[i][j] = self.symbols[len(self.symbols) - 1]
+
+        return draw_canvas
--- Turtle/main.py
+++ Turtle/main.py
@@ -1,16 +1,46 @@
-# This is a sample Python script.
+from Turtle import turtle
+import unittest
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+from solution import SimpleCanvas
+from solution import Turtle
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+class SolutionTest(unittest.TestCase):
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def setUp(self):
+        self.turtle = Turtle(3, 3)
+        self.turtle.spawn_at(0, 0)
+
+    def test_turtle_can_move(self):
+        self.turtle.move()
+        self.assertEqual([[1, 1,  0], [0, 0 ,0], [0, 0, 0]], self.turtle.canvas)
+
+    def test_turtle_can_turn_right(self):
+        self.turtle.turn_right()
+        self.turtle.move()
+        self.assertEqual([[1, 0,  0], [1, 0 ,0], [0, 0, 0]], self.turtle.canvas)
+
+    def test_turtle_can_turn_left(self):
+        self.turtle.turn_left()
+        self.turtle.move()
+        self.assertEqual([[1, 0,  0], [0, 0 ,0], [1, 0, 0]], self.turtle.canvas)
+
+    def test_canvas_drawn_correctly(self):
+        for i in range(9):
+            self.turtle.move()
+        self.turtle.turn_right()
+        for i in range(4):
+            self.turtle.move()
+        self.turtle.turn_left()
+        self.turtle.move()
+        self.turtle.move()
+        self.turtle.turn_right()
+        self.turtle.move()
+        canvas = SimpleCanvas(self.turtle.canvas, [' ', '*', '@', '#'])
+        canvas.draw()
+
+        self.assertEqual("#@@\n@**\n* *", canvas.draw())
+
+
+if __name__ == "__main__":
+    unittest.main()
